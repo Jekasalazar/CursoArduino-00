a032e22db312a506d41b8c322c3b581c56f9cdd3
Alex Vargas Benamburg <avargas@vbalex.com>
2016-06-01 10:54:09 -0600

    Modificacion en ejemplo de escritura analogica

--- _07-Escritura_Analogica/_07-Escritura_Analogica.ino
+++ _07-Escritura_Analogica/_07-Escritura_Analogica.ino
@@ -1,14 +1,18 @@
+int ledAzul = 3;
+int ledVerde = 5;
+int ledRojo = 6;
+
 void setup() {
-  pinMode(5,OUTPUT);
+  pinMode(ledAzul,OUTPUT);
 }
 
 void loop() {
-  for(int i=0;i<255:++i){
-    analogWrite(5,i);
+  for(int i=0; i<255; ++i){
+    analogWrite(ledAzul,i);
     delay(10);
   }
-  for(int i=255;i>0;--0){
-    analogWrite(5,i);
+  for(int i=255; i>0; --i){
+    analogWrite(ledAzul,i);
     delay(10);
   }
 }
