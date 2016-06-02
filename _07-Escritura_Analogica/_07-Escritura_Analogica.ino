0d9d2294b6d312c06152aa3292b82e6abb8d3ef5
Alex Vargas Benamburg <avargas@vbalex.com>
2016-06-01 19:41:05 -0600

    Agregado mas leds

--- _07-Escritura_Analogica/_07-Escritura_Analogica.ino
+++ _07-Escritura_Analogica/_07-Escritura_Analogica.ino
@@ -2,17 +2,30 @@ int ledAzul = 3;
 int ledVerde = 5;
 int ledRojo = 6;
 
+int estadoAzul = 0;
+int estadoRojo = 100;
+int estadoVerde = 200;
+
 void setup() {
   pinMode(ledAzul,OUTPUT);
+  pinMode(ledVerde,OUTPUT);
+  pinMode(ledRojo,OUTPUT);
 }
 
 void loop() {
-  for(int i=0; i<255; ++i){
-    analogWrite(ledAzul,i);
-    delay(10);
-  }
-  for(int i=255; i>0; --i){
-    analogWrite(ledAzul,i);
-    delay(10);
-  }
+ analogWrite(ledAzul, estadoAzul);
+ analogWrite(ledVerde, estadoVerde);
+ analogWrite(ledRojo, estadoRojo);
+ estadoAzul = estadoAzul + 2;
+ estadoVerde = estadoVerde + 3;
+ estadoRojo = estadoRojo + 1;
+ if(estadoAzul > 255){
+  estadoAzul = 0;
+ }
+ if(estadoVerde > 255){
+  estadoVerde = 0;
+ }
+ if(estadoRojo > 255){
+  estadoRojo = 0;
+ }
 }
